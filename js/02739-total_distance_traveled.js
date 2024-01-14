5fb408618b932f040f0c93080ae3d168504d6881
vulbyte <vulbyt3@gmail.com>
2024-01-14 00:33:19 -0800

    completed 02739

--- js/02739-total_distance_traveled.js
+++ js/02739-total_distance_traveled.js
@@ -1,56 +1,67 @@
-/*
-A truck has two fuel tanks. You are given two integers, mainTank representing the fuel present in the main tank in liters and additionalTank representing the fuel present in the additional tank in liters.
-
-The truck has a mileage of 10 km per liter. Whenever 5 liters of fuel get used up in the main tank, if the additional tank has at least 1 liters of fuel, 1 liters of fuel will be transferred from the additional tank to the main tank.
-
-Return the maximum distance which can be traveled.
-
-Note: Injection from the additional tank is not continuous. It happens suddenly and immediately for every 5 liters consumed.
-
- 
-
-Example 1:
-
-Input: mainTank = 5, additionalTank = 10
-Output: 60
-Explanation: 
-After spending 5 litre of fuel, fuel remaining is (5 - 5 + 1) = 1 litre and distance traveled is 50km.
-After spending another 1 litre of fuel, no fuel gets injected in the main tank and the main tank becomes empty.
-Total distance traveled is 60km.
-Example 2:
-
-Input: mainTank = 1, additionalTank = 2
-Output: 10
-Explanation: 
-After spending 1 litre of fuel, the main tank becomes empty.
-Total distance traveled is 10km.
-*/
-
+//this is my solution, which ended up falling short. i attempted to do it in a systematic way incase things could be added or removed systematically.
+//i was too stupid for this approach
 /**
 * @param {number} mainTank
 * @param {number} additionalTank
 * @return {number}
 */
+/*
+var distanceTraveled = function(mainTank, additionalTank) {
+let distance = 0 * 1;
+let additionalDistance = 0 * 1;
+
+while (mainTank > 1) {
+    console.log("main tank, additional tank: ", mainTank, ", ", additionalTank);
+
+    if (distance % 5 == 0) {
+        additionalTank = additionalTank - 1;
+        additionalDistance = additionalDistance + 1;
+        mainTank = mainTank - 1;
+        distance = distance + 1;
+    }
+    else {
+        mainTank = mainTank - 1;
+        distance = distance + 1;
+    }
+    console.log("distance = ", distance);
+}
+
+distance++;
+
+console.log("final distance = ", (distance + additionalDistance) * 10);
+return (distance + additionalDistance) * 10;
+}
+*/
+
+//this was an example solution given, it does what the obvious thing in a decently obvious way i didn't think of
+/**
+ * @param {number} mainTank
+ * @param {number} additionalTank
+ * @return {number}
+ */
 var distanceTraveled = function(mainTank, additionalTank) {
     let distance = 0;
 
-    while (mainTank > 0) {
-        console.log("main tank, additional tank: ", mainTank, additionalTank);
-        //check for refuel
-        if (mainTank % 5 == 0 && additionalTank > 0) {
-            additionalTank = additionalTank - 1;
-            mainTank = mainTank - 2;
-            distance = distance + 2;
-        }
-        else {
-            //consume fuel
-            mainTank = mainTank - 1;
+    if (mainTank < 5) {
+        let remainingFuel = mainTank % 5;
+        return remainingFuel * 10;
+    }
+
+    while (mainTank > 0 || additionalTank > 0) {
+        if (mainTank >= 5) {
+            distance += 50;
+            mainTank -= 5;
+
+            if (additionalTank > 0) {
+                mainTank += 1;
+                additionalTank -= 1;
+            }
+        } else {
+            let remainingFuel = mainTank % 5;
+            distance += remainingFuel * 10;
+            break;
         }
     }
 
-    console.log(distance);
-}
-
-distanceTraveled(10, 5);
-
-distanceTraveled(2, 1);
+    return distance;
+};
