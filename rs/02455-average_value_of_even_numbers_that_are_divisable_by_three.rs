c15a2a20f56a7c048d56619ffa760071eb7d1c36
vulbyte <vulbyt3@gmail.com>
2024-01-17 00:04:45 -0800

    completed 02455 in rust

--- rs/02455-average_value_of_even_numbers_that_are_divisable_by_three.rs
+++ rs/02455-average_value_of_even_numbers_that_are_divisable_by_three.rs
@@ -1,12 +1,58 @@
-//this
-var averageValue = function(nums) {
-    let sum = 0;
-    let count = 0;
-    for (let n of nums) {
-        if (n % 6 === 0) {
-            sum += n;
-            count++;
+// struct Solution;
+//
+// impl Solution {
+//     pub fn average_value(nums: Vec<i32>) -> i32 {
+//         let mut sum = 0;
+//         let mut count = 0;
+//         for n in nums.iter() {
+//             if n % 6 == 0 {
+//                 sum = sum + n;
+//                 count = count + 1;
+//             };
+//         }
+//         return sum / count;
+//     }
+// }
+//
+// fn main() {
+//     let test1: [i32; 6] = [1, 3, 6, 10, 12, 15];
+//     let test2: [i32; 5] = [1, 2, 4, 7, 10];
+//
+//     let mut result = Solution::average_value(test1);
+//
+//     println!("average value: {}", Solution::average_value(test1));
+//
+//     println!("average value: {}", Solution::average_value(test2));
+// }
+
+
+
+struct Solution;
+
+impl Solution {
+    pub fn average_value(nums: Vec<i32>) -> i32 {
+        let mut sum = 0;
+        let mut count = 0;
+        for n in nums.iter() {
+            if n % 6 == 0 {
+                sum = sum + n;
+                count = count + 1;
+            };
         }
+        if count == 0 {
+            // Avoid division by zero
+            return 0.0 as i32;
+        }
+        return (sum as f64 / count as f64) as i32;
     }
-    return sum === 0 ? sum : Math.floor(sum / count);
-};
+}
+
+fn main() {
+    let test1: [i32; 6] = [1, 3, 6, 10, 12, 15];
+    let test2: [i32; 5] = [1, 2, 4, 7, 10];
+
+    println!("average value: {}", Solution::average_value(test1.to_vec()));
+
+    println!("average value: {}", Solution::average_value(test2.to_vec()));
+}
+
