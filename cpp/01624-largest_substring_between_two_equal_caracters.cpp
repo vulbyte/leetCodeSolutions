e710c827ecca44abd813d8b028745df4e81eabab
vulbyte <vulbyt3@gmail.com>
2024-02-28 12:24:31 -0800

    updated 01624 one more time. wanted to insure that i understood the code

--- cpp/01624-largest_substring_between_two_equal_caracters.cpp
+++ cpp/01624-largest_substring_between_two_equal_caracters.cpp
@@ -80,23 +80,110 @@
 
 // TESTED THE "LEAST MEMORY" SOLUTION, AND THIS WAS THE FASTEST RUNTIME.
 // HONESTLY PROBABLY A GLITCH BUT YEH
+// KNEE JERK SOLUTION, TOO SLOW BECAUSE O(n^2)
+// class Solution {
+// public:
+//     int maxLengthBetweenEqualCharacters(string s) {
+//         int maxGap = -1;
+//         int gap = -1;
+// 
+//         for (int i = 0; i < s.size(); i++) {
+//             gap = findGap(s, s[i], i);
+//             if (gap > maxGap){
+//                 maxGap = gap;
+//             }
+//         }
+// 
+//         if (maxGap == -1) {
+//             return -1;
+//         }
+// 
+//         return maxGap-1;
+//     }
+//     int findGap(string s, char c, int i){
+//         for (int j = s.size(); j > i; --j) {
+//             if (s[j] == c){
+//                 cout << "char gap = " << abs(i-j) << endl;
+//                 return abs(i-j);
+//             }
+//         }
+//         cout << "char gap = {-1}" << endl;
+//         return -1;
+//     }
+// };
+
+// "mgntdygtxrvxjnwksqhxuxtrv"
+//     ^                  ^
+
+// GIVEN SOLUTION, TESTED TO COMPARE SPEED, DIFFERENCE WAS NEGLIGABLE
+// class Solution {
+// public:
+//     int maxLengthBetweenEqualCharacters(string s) {
+//         //character an unordered map named first index
+//         unordered_map<char, int> firstIndex;
+//         //init ans
+//         int ans = -1;
+//         
+//         // until i == s.size();
+//         for (int i = 0; i < s.size(); i++) {
+//             //if first and last index of char
+//             if (firstIndex.find(s[i]) != firstIndex.end()) {
+//                 //if found, return 
+//                 ans = max(ans, i - firstIndex[s[i]] - 1);
+//             } else {
+//                 // increment the index
+//                 firstIndex[s[i]] = i;
+//             }
+//         }
+// 
+//         // return
+//         return ans;
+//     }
+// };
+
+// ATTEMPTED ANOTHER "FASTER" SOLUTION, WAS NEGLIGABLE FROM THE LAST
+// class Solution {
+// public:
+//     int maxLengthBetweenEqualCharacters(string s) {
+//         unordered_map<char, vector<int>> mp;
+//         for(int i = 0; i < s.size(); i++){
+//             mp[s[i]].emplace_back(i);
+//         }
+//         int max = -1;
+//         int n = mp.size();
+//         for(int i = 0; i< n; i++){
+//             if(mp[s[i]].size() > 1 && max < mp[s[i]][mp[s[i]].size() - 1] - mp[s[i]][0]){
+//                 max = mp[s[i]][mp[s[i]].size() - 1] - mp[s[i]][0] - 1;
+//             }
+//         }
+//         return max;
+//     }
+// };
+
+// TESTED THE "LEAST MEMORY" SOLUTION, AND THIS WAS THE FASTEST RUNTIME.
+// HONESTLY PROBABLY A GLITCH BUT YEH
 class Solution {
 public:
     int maxLengthBetweenEqualCharacters(string s) {
         // pre allocate size
-        int n = s.size();
+        int s = s.size();
 
         // result defaults to -1
         int res = -1;
 
         //for each 
-        for (int l = 0; l < n - 1; ++l){
-            int r = n - 1;
+        for (int l = 0; l < s - 1; ++l){
+            // right = size-1
+            int r = s - 1;
+            //while left < right
             while (l < r){
+                //if s[l] == s[r] {res = max(res, r-l-1)} 
                 if (s[l] == s[r]) res = max(res, r - l - 1);
+                // r = r-1;
                 --r;
             }
         }
+        //return result
         return res;
     }
-}
+};
